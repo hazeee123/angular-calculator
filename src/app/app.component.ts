45147d5514f333e028800ba394062773bd3b89f6
Hazel Lopez <hazel.lopez@usjr.edu.ph>
2021-10-02 19:32:30 +0800

    Added Angular Calculator Activity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-calculator';
+  input1 = 0;
+  input2 = 0;
+  result = 0;
+
+  getInput1(value: string){
+    const parsedInt = parseInt(value);
+
+    if(!isNaN(parsedInt)) {
+      this.input1 = parsedInt;
+      console.log(this.input1);
+    }
+  }
+
+  getInput2(value: string){
+    const parsedInt = parseInt(value);
+
+    if(!isNaN(parsedInt)) {
+      this.input2 = parsedInt;
+      console.log(this.input2);
+    }
+  }
+
+  toAdd() {
+    console.log(this.result = this.input1 + this.input2)
+  }
+
+  toSubtract() {
+    console.log(this.result = this.input1 - this.input2)
+  }
+
+  toMultiply() {
+    console.log(this.result = this.input1 * this.input2)
+  }
+
+  toDivide() {
+    console.log(this.result = this.input1 / this.input2)
+  }
+
 }
